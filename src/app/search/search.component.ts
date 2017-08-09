10410cc47efbe8c1db706037b4062250a126306f
Shubham Singh <mynameshubham@gmail.com>
2017-08-09 13:33:16 +0400

    add show design in place

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,6 +14,8 @@ export class SearchComponent implements OnInit {
     show: string = '';
     shows = [];
     myShows = [];
+    statusColor: string;
+    colors = [];
 
     getposts(junk: string) {
         return this.http.get(this.baseUrl+'/search/shows?q='+junk).map(res => res.json());
@@ -21,6 +23,10 @@ export class SearchComponent implements OnInit {
 
     search(junk: string){
         this.getposts(junk).subscribe((posts) => {
+            for(let i = 0; i < posts.length; i++){
+                if(posts[i].show.image == null)
+                    posts[i].show.image = 'http://www.downloadclipart.net/large/1197-blue-rectangle-white-up-arrow-design.png';
+            }
             this.shows = posts;
             console.log(this.shows);
         })
@@ -28,13 +34,16 @@ export class SearchComponent implements OnInit {
 
     subscribe(id: number) {
         this.myShows.push(id);
-        console.log(this.myShows);
     }
 
     constructor(public http: Http) { }
 
     ngOnInit() {
         document.getElementById('focusThis').focus();
+        this.search('friends');
+        this.colors['Running'] = "#1db954";
+        this.colors['Ended'] = "#f44336";
     }
+
 
 }
