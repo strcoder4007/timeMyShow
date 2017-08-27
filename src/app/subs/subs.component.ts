50536887741d23bd5d2d3180a9a306931eb9ff53
Shubham Singh <mynameshubham@gmail.com>
2017-08-28 01:19:01 +0400

    next episode implemented

--- src/app/subs/subs.component.ts
+++ src/app/subs/subs.component.ts
@@ -10,21 +10,47 @@ import 'rxjs/add/operator/map';
 export class SubsComponent implements OnInit {
     myShows = [];
     myIds: any;
+    junk = [];
 
     constructor(public http: Http) { }
 
-    getposts(id: string) {
+    getShows(id: string) {
         return this.http.get('http://api.tvmaze.com/shows/'+id).map(res => res.json());
     }
 
+    getEpisodes(id: string) {
+        return this.http.get('http://api.tvmaze.com/shows/'+id+'/episodes').map(res => res.json());
+    }
+    
     ngOnInit() {
         this.myIds = localStorage.getItem('myShows').split(',');
         if(this.myIds.length) {
             for(let i = 0; i < this.myIds.length; i++) {
-                this.getposts(this.myIds[i]).subscribe((posts) => {
+                this.getShows(this.myIds[i]).subscribe((posts) => {
                     this.myShows.push(posts);
                 })
+            this.getEpisodes(this.myIds[i]).subscribe((posts) => {
+                this.junk.push(posts);
+                let today = new Date();
+                let month = "";
+                month += today.getMonth();
+                if(month.length == 1)
+                    month = '0'+month;
+                let day = "";
+                day += today.getDay();
+                if(day.length == 1)
+                    day = '0'+day;
+                let myDay = today.getFullYear()+'-'+month+'-'+day;
+                let airdate = posts[posts.length-1].airdate;
+                if(myDay > airdate) {
+                    console.log("show ended on "+airdate);
+                }
+                else {
+                    console.log("next episode on: "+ airdate);
+                }
+            })
             }
+            console.log(this.junk);
             console.log(this.myShows);
         }
     }
